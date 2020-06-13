c0fae86795337a8920e4c2b43f3aa48c7737c163
Abel Kalum <chumbabel@gmail.com>
2020-06-13 17:02:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +8,43 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params[:article])
+    erb :show
+  end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect '/articles'
   end
 end
