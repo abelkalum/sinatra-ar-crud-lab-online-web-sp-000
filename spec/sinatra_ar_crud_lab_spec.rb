37a1c0c55c8e8762c78fe241a39a694be0d5259f
Abel Kalum <chumbabel@gmail.com>
2020-06-13 17:24:37 +0000

    Automatically backed up by Learn

--- spec/sinatra_ar_crud_lab_spec.rb
+++ spec/sinatra_ar_crud_lab_spec.rb
@@ -93,7 +93,7 @@ describe "Magazine App" do
       fill_in :content, :with => "this is the best article ever written"
 
       page.find(:css, "[type=submit]").click
-      expect(Article.all.count).to eq(1)
+      expect(Article.all.count).to eq(2)
       expect(Article.last.title).to eq("second article")
     end
 
